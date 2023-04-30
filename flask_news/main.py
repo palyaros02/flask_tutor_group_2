3d6d3b176bec25aa9e390443d8f0683e45b6ce72
Yaroslav Palamarchuk <palyaros02@gmail.com>
2023-04-30 19:31:25 +0300

    ADD: index html

--- flask_news/main.py
+++ flask_news/main.py
@@ -3,9 +3,27 @@ from flask import Flask
 
 app = Flask(__name__)
 
+index_temlate = """\
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Главная страница</title>
+    </head>
+    <body>
+        <h1>Главная страница</h1>
+        <hr>
+        <p>Привет, <b>Flask</b>!</p>
+        <p><a href="/news">Новости</a></p>
+        <p><img src="https://www.python.org/static/community_logos/python-logo-master-v3-TM.png"></p>
+    </body>
+</html>
+"""
+
+
 @app.route('/')
-def index():
-    return "Главная страница"
+def index() -> str:
+    return index_temlate
 
 @app.route('/news')
 def news():
