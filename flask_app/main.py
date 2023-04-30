3524da84a51dfe78ca82c6ae3da17b7b17756c87
Shchukaa <mr_shchuka@mail.ru>
2023-04-30 18:27:39 +0300

    test

--- flask_app/main.py
+++ flask_app/main.py
@@ -5,31 +5,38 @@ import requests
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return "Главная страница"
+
 
 @app.route('/news')
 def news():
     return "Новости"
 
+
 @app.route('/news_detail/1')
 def news1():
     return "Новость 1"
+
 
 @app.route('/news_detail/2')
 def news2():
     return "Новость 2"
 
+
 def get_finonacci(n):
     numbers = [1, 1]
     for i in range(2, n):
-        numbers.append(numbers[i-1] + numbers[i-2])
+        numbers.append(numbers[i - 1] + numbers[i - 2])
     return numbers
+
 
 @app.route('/fibonacci')
 def fibonacci():
     return ' '.join(map(str, get_finonacci(n)))
+
 
 def get_course() -> str:
     response = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
@@ -37,6 +44,7 @@ def get_course() -> str:
     for code, data in response['Valute'].items():
         result += f'{data["Nominal"]} {data["Name"]} стоит {data["Value"]} руб.<br>'
     return result
+
 
 @app.route('/money')
 def money():
@@ -50,10 +58,16 @@ def get_data():
             data.append(line.strip())
     return data
 
+
 @app.route('/random')
 def citate():
     data = get_data()
     return random.choice(data)
+
+
+@app.route('/total/<int:a>/<int:b>')
+def total(a, b):
+    return f'a + b = {a + b}'
 
 
 if __name__ == '__main__':
