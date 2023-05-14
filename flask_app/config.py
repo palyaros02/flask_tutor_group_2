008783f7762caa63fdd88a796f9ae2b314e050f3
Yaroslav Palamarchuk <palyaros02@gmail.com>
2023-05-14 19:38:22 +0300

    flask app: add db

--- flask_app/config.py
+++ flask_app/config.py
@@ -2,3 +2,5 @@ from flask import Flask, url_for
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1238765ruhvsdjhft8hy2t345qjkhgfbi87yasdftbof8aw37495tb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///feedback.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
--- flask_app/main.py
+++ flask_app/main.py
@@ -1,11 +1,26 @@
 from cgitb import text
 
 from flask import Flask, redirect, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from practice import app
 from wtforms import (EmailField, SelectField, StringField, SubmitField,
                      TextAreaField)
 from wtforms.validators import DataRequired, Email, Optional
+
+db = SQLAlchemy(app)
+
+class Feedback(db.Model): # type: ignore
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(64))
+    rating = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<Feedback({self.id=}, {self.name=}, {self.text=}, {self.email=}, {self.rating=})>'
+
+db.create_all()
 
 context = {'title': 'Заголовок',
            'text': 'Текст'}
@@ -21,14 +36,18 @@ class FeedbackForm(FlaskForm):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = FeedbackForm()
+    feedbacks = Feedback.query.all()
     if form.validate_on_submit():
-        name = form.name.data
-        text = form.text.data
-        email = form.email.data
-        rating = form.rating.data
-        print(name, text, email, rating)
+        feedback = Feedback(
+            name=form.name.data,
+            text=form.text.data,
+            email=form.email.data,
+            rating=form.rating.data,
+        )
+        db.session.add(feedback)
+        db.session.commit()
         return redirect('/')
-    return render_template('index.html', form=form)
+    return render_template('index.html', form=form, feedbacks=feedbacks)
 
 
 @app.route('/news')
