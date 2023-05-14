61fc8aff46f4498e47985b80d90d8444b48632ad
Yaroslav Palamarchuk <palyaros02@gmail.com>
2023-05-14 19:10:31 +0300

    add db

--- flask_news/main.py
+++ flask_news/main.py
@@ -1,32 +1,28 @@
+from datetime import datetime
+
 from flask import Flask, redirect, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'asdasdasdae2weqwe123'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///news.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-news = [{'title': 'Удивительное событие в школе',
-         'text': 'Вчера в местной школе произошло удивительное событие - все '
-                 'ученики одновременно зевнули на уроке математики. '
-                 'Преподаватель был так поражен этим коллективным зевком, '
-                 'что решил отменить контрольную работу.'},
-        {'title': 'Случай в зоопарке',
-         'text': 'В зоопарке города произошел необычный случай - ленивец '
-                 'решил не лениться и взобрался на самое высокое дерево в '
-                 'своем вольере. Посетители зоопарка были поражены такой '
-                 'активностью и начали снимать ленивца на видео. В итоге он '
-                 'получил свой собственный канал на YouTube, где он размещает '
-                 'свои приключения.'},
-        {'title': 'Самый красивый пёс',
-         'text': 'Сегодня в парке прошел необычный конкурс - "Самый красивый '
-                 'пёс". Участники конкурса были так красивы, что судьи не '
-                 'могли выбрать победителя. В итоге, конкурс был объявлен '
-                 'ничейным, а участники получили награды за участие, '
-                 'в том числе - пакетики конфет и игрушки в виде косточек. '
-                 'Конкурс вызвал большой интерес у посетителей парка, '
-                 'и его решили повторить в более масштабном формате.'}]
+db = SQLAlchemy(app)
 
+class News(db.Model): # type: ignore
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False, unique=True)
+    text = db.Column(db.Text, nullable=False)
+    created_date = db.Column(db.DateTime, default=datetime.utcnow())
+
+    def __repr__(self):
+        return f'<News({self.id=}, {self.title=}, {self.text=}, {self.created_date=})>'
+
+db.create_all()
 
 class NewsForm(FlaskForm):
     title = StringField(
@@ -47,25 +43,27 @@ class NewsForm(FlaskForm):
 
 @app.route('/')
 def index():
+    news = News.query.all()
     return render_template('index.html',
                            news=news[::-1])
 
 
 @app.route('/news_detail/<int:id>')
 def news_detail(id):
-    title = news[id]['title']
-    text = news[id]['text']
+    news_item = News.query.get(id)
     return render_template('news_detail.html',
-                           title=title,
-                           text=text)
+                           news=news_item)
 
 @app.route('/add_news', methods=['GET', 'POST'])
 def add_news():
     form = NewsForm()
     if form.validate_on_submit():
-        title = form.title.data
-        text = form.text.data
-        news.append({'title': title, 'text': text})
+        news_item = News(
+            title=form.title.data,
+            text=form.text.data
+        )
+        db.session.add(news_item)
+        db.session.commit()
         return redirect(url_for('index'))
     return render_template('add_news.html', form=form)
 
